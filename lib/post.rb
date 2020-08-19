18a32ad123c43099a5707f31b19489db82be5e23
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-19 23:24:03 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -3,9 +3,19 @@ class Post
 @@all = []
 
 
+  def initialize(title)
+    @title = title
+    #temporary. i'm not sure if they want it to be called title
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
   def self.all
     @@all
   end
 
-  
+
 end
