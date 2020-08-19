05d859a2f3c3ab705be824f365aa60e6445453ae
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-19 23:20:49 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,18 @@
 class Author
-
+  attr_accessor :name
 def initialize(name)
   @name = name
+end
+
+
+
+  def posts
+    Post.all.select do | song |
+      post.author == self
+    end
+  end
+
+
+
+
 end
